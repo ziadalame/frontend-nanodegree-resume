63ceb82facae09cc3b2dbfdaae71ed7d17bad442
ziad <ziadalame@gmail.com>
2017-05-26 04:46:14 +0300

    added education section to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -34,7 +34,7 @@ var bio = {
         var formattedLocation = HTMLlocation.replace(replacableDataString, bio.contacts.location);
         var formattedBioPic = HTMLbioPic.replace(replacableDataString, bio.biopic);
         var formattedWelcomeMessage = HTMLwelcomeMsg.replace(replacableDataString, bio.welcomeMessage);
-        
+
         // For simplicity, concatenate all contact HTML with each other
         var formattedContact = formattedMobile + formattedEmail + formattedGithub + formattedTwitter + formattedLocation;
 
@@ -58,6 +58,84 @@ var bio = {
     }
 };
 
+var education = {
+    schools: [
+        {
+            'name': 'Arab Open University',
+            'location': 'Beirut, Lebanon',
+            'degree': 'Bachelor of Science',
+            'majors': ['Computer Science'],
+            'dates': '2011 - Present',
+            'url': 'http://www.aou.edu.lb'
+        }
+    ],
+    onlineCourses: [
+        {
+            'title': 'Front End Nanodegree',
+            'school': 'Udacity',
+            'dates': 'May 2017',
+            'url': 'https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001'
+        }
+    ],
+    display: function () {
+        // Check if education has at least one school or one online course to know whether to keep or remove education title
+        if (education.schools.length || education.onlineCourses.length) {
+            for (var i = 0; education.schools.length > i; i++) {
+                // Add content to HTML
+                var formattedName = HTMLschoolName.replace(replacableDataString, education.schools[i].name);
+                var formattedDegree = HTMLschoolDegree.replace(replacableDataString, education.schools[i].degree);
+                var formattedSchoolDates = HTMLschoolDates.replace(replacableDataString, education.schools[i].dates);
+                var formattedLocation = HTMLschoolLocation.replace(replacableDataString, education.schools[i].location);
 
-// Add bio to page
+                // Add education entry div
+                $('#education').append(HTMLschoolStart);
+
+                // append school to education entries
+                $('.education-entry:last').append(formattedName + formattedDegree);
+                $('.education-entry:last').append(formattedSchoolDates);
+                $('.education-entry:last').append(formattedLocation);
+
+                // loop over majors
+                for (var j = 0; education.schools[i].majors.length > j; j++) {
+                    // add content to HTML tag
+                    var formattedMajor = HTMLschoolMajor.replace(replacableDataString, education.schools[i].majors[j]);
+                    // Apped content
+                    $('.education-entry:last').append(formattedMajor);
+                }
+            }
+        } else {
+            // remove the education h2 tag by emptying the div tag to avoid showing an empty education section
+            $('#education').html('');
+        }
+
+        // Check if education has at least one online course to add the title
+        if (education.onlineCourses.length) {
+            $('#education').append(HTMLonlineClasses);
+            
+            // loop over online courses
+            for (var i = 0; education.onlineCourses.length > i; i++) {
+                // add content to HTML tags
+                var formattedTitle = HTMLonlineTitle.replace(replacableDataString, education.onlineCourses[i].title).replace('#', education.onlineCourses[i].url);
+                var formattedSchool = HTMLonlineSchool.replace(replacableDataString, education.onlineCourses[i].school);
+                var formattedOnlineDates = HTMLonlineDates.replace(replacableDataString, education.onlineCourses[i].dates);
+                var formattedUrl = HTMLonlineURL.replace(replacableDataString, education.onlineCourses[i].url);
+                
+                // Add an education entry div
+                $('#education').append(HTMLschoolStart);
+                
+                // Add education data to entry
+                $('.education-entry:last').append(formattedTitle + formattedSchool);
+                $('.education-entry:last').append(formattedOnlineDates);
+                $('.education-entry:last').append(formattedUrl);
+            }
+        }
+    }
+};
+
+// TODO: Add work
+
+// TODO: Add projects
+
+// Add sections to page
 bio.display();
+education.display();
